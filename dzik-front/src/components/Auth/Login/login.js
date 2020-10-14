b3d4a0e9070b0d37642cf07254b9519c3b89c4b3
Paweł Kubala <quolumbo@gmail.com>
2020-10-14 16:30:15 +0200

    chore: DZIK-36 added fb, google, instagram icons

--- dzik-front/src/components/Auth/Login/login.js
+++ dzik-front/src/components/Auth/Login/login.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 import classes from "./login.module.scss";
 import authClasses from "../auth.module.scss";
-import Button from '../../Button/button'
-
+import Button from "../../Button/button";
+import facebookLogo from "../../../assets/images/auth_icons/facebook_logo.svg";
+import googleLogo from "../../../assets/images/auth_icons/google_logo.svg";
+import instagramLogo from "../../../assets/images/auth_icons/instagram_logo.svg";
 
 const login = () => {
   return (
@@ -15,11 +16,23 @@ const login = () => {
         <input type="password" placeholder="Hasło" required />
         <Button>Zaloguj</Button>
       </form>
+      <div className={classes.imgContainer}>
+        <div className={classes.logoImage}>
+          <img src={facebookLogo} alt="facebook" />
+        </div>
+        <div className={classes.logoImage}>
+          <img src={googleLogo} alt="google" />
+        </div>
+        <div className={classes.logoImage}>
+          <img src={instagramLogo} alt="instagram" />
+        </div>
+      </div>
+
       <div className={classes.notRegistered}>
-        <p>
-          Nie należysz jeszcze do stada?
-        </p>
-          <Link className={classes.buttonRegister} to="/auth/register">Zarejestruj się!</Link>{" "}
+        <p>Nie należysz jeszcze do stada?</p>
+        <Link className={classes.buttonRegister} to="/auth/register">
+          Zarejestruj się!
+        </Link>{" "}
       </div>
     </div>
   );
