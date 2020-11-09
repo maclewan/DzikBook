c229916224fb3a7b927cfdd0f29b65c65263042e
mihal09 <mjanik1@op.pl>
2020-11-09 23:52:17 +0100

    add wall serializers

--- backend/dzikbook/wall/models.py
+++ backend/dzikbook/wall/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from media.models import Photo
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.TextField()
     visibility = models.BooleanField(default=True)
-    image = models.ForeignKey(Photo, blank=True, null=True, on_delete=models.SET_NULL)
+
+    #image = models.ForeignKey(Photo, blank=True, null=True, on_delete=models.SET_NULL)
+    image = models.CharField(max_length=200, null=True, blank=True)
     # TODO: wymyslec sensowny sposob na trzymanie roznych typow wpisow
     content = models.TextField(null=True)
