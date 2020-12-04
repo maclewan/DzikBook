8cd06f2ab41b2da8c91b2d2e765d01d794067606
mihal09 <mjanik1@op.pl>
2020-12-04 23:44:25 +0100

    feat: add working image upload

--- backend/dzikbook/media/urls.py
+++ backend/dzikbook/media/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import (
     PhotoManagementView,
-    VideoManagementView,
     ProfilePhotoView,
     SigInUserProfilePhotoView
 )
@@ -9,10 +8,9 @@ from .views import (
 urlpatterns = [
     path('photo/', PhotoManagementView.as_view(), name='upload_photo'),
     path('photo/<int:photo_id>/', PhotoManagementView.as_view(), name='photo_management'),
-    path('video/', VideoManagementView.as_view(), name='upload_video'),
-    path('video/<int:video_id>/', VideoManagementView.as_view(), name='video_management'),
     path('profile/', SigInUserProfilePhotoView.as_view(), name='sig_in_user_profile_photo'),
-    path('profile/<int:user_id>/', ProfilePhotoView.as_view(), name='profile_photo')
+    path('profile/<int:profile_photo_id>/', SigInUserProfilePhotoView.as_view(), name='profile_photo'),
+    path('profile/user/<int:user_id>/', ProfilePhotoView.as_view(), name='profile_photo')
 ]
 
 
