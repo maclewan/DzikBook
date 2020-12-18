667f0f15043f2c42a3fd9d8709c9d99f21cd0183
Piotr Szymański <piotrszym159@gmail.com>
2020-12-18 00:08:04 +0000

    Revert "Merge branch 'revert-4b17beaa' into 'develop'"
    This reverts merge request !39

--- backend/dzikbook/users/models.py
+++ backend/dzikbook/users/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,4 +7,7 @@ class UserData(models.Model):
     first_name = models.CharField(null=True, blank=False, max_length=40)
     last_name = models.CharField(null=True, blank=False, max_length=40)
     gym = models.TextField(null=True, blank=False)
+    birth_date = models.DateField(null=True, blank=False)
+    sex = models.CharField(null=True, blank=False, max_length=25)
+    job = models.CharField(null=True, blank=False, max_length=40)
     additional_data = models.TextField(null=True, blank=False)
--- backend/dzikbook/users/urls.py
+++ backend/dzikbook/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     SignedInUserDataView,
     UserDataView,
-    SearchView, MultipleUsersDataView
+    SearchView, MultipleUsersDataView, CreateNewUserView
 )
 
 urlpatterns = [
@@ -10,7 +10,8 @@ urlpatterns = [
     path('data/<int:id>/', UserDataView.as_view(), name='user_data'),
     path('search/', SearchView.as_view(), name='search'),
     #Internal
-    path('multi/', MultipleUsersDataView.as_view(), name='multiple_user_data')
+    path('multi/', MultipleUsersDataView.as_view(), name='multiple_user_data'),
+    path('data/new/', CreateNewUserView.as_view(), name='sig_in_user_data')
 ]
 
 
